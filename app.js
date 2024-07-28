eae231ff4bdd04dc0579cfe1d36881e486c68049
Amit Cohen <amit.c@cheq.ai>
2024-07-28 16:14:45 +0300

    rti response received and passed to client

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// env: staging
+// adserver env: staging
 // net id 8914
 // search id 50193
-// bot useragent string: Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)
+
+// invoke: npm run start -> go to localhost:5000
 
 const express = require("express");
 const app = express();
-const { rti, eventsTypes } = require("@cheq.ai/cheq-middlewares");
 const path = require("path");
+const { rti, eventsTypes } = require("@cheq.ai/cheq-middlewares");
 const { apiEndpoints } = require("@cheq.ai/cheq-middlewares/config");
-
 const PORT = process.env.PORT || 5000;
 const options = {
   apiKey: "ca9635f4-81fc-4dc4-9f2f-d4a78680787f",
@@ -16,30 +16,35 @@ const options = {
   apiEndpoint: apiEndpoints.DEV,
   mode: "blocking",
   threatTypesCodes: {
-    blockRedirect: [2, 3, 6, 10, 11, 16, 18,29], // 11 = FALSE REPRESENTATION -> block & redirect (redirectUrl)
-    captcha: [4, 5, 13, 14, 15, 17, 7], // 7 = DISABLED JS -> block & navigate to captcha.html (callback)
+    blockRedirect: [2, 3, 6, 11, 16, 18, 29, 10], // threat_type 10 = Malicious Bots / threat_type 3 = Automation tool -> bot useragent string: Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html) -> block & redirect (redirectUrl)
+    captcha: [4, 5, 13, 15, 17, 7, 14], // threat_type 14 = VPN -> navigate to captcha.html (callback)
   },
-  redirectUrl: "https://google.com",
+  redirectUrl: "https://localhost:5000/redirect.html",
   callback: function (req, res, next) {
     res.sendFile(path.join(__dirname, "./frontend", "captcha.html"));
   },
 };
 
+// Setup
 const middleware = rti(options);
-
-// Serve static files from the frontend directory
 app.use(express.static(path.join(__dirname, "./frontend")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "frontend"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./frontend", "index.html"));
-});
-
+// Routes
 app.get("/subscribe", middleware(eventsTypes.SUBSCRIBE), function (req, res) {
-  console.log(res.getHeaders());
+  const rtiRes = res.locals.rtiRes;
+  const rtiResString = JSON.stringify(rtiRes, null, 2); // Convert object to a pretty-printed JSON string
 
-  res.send("SUBSCRIBE PAGE");
+  res.render("subscribe", { rtiResString });
 });
 
+
+app.get("/redirect", (req, res) => {
+  res.sendFile(__dirname + "/frontend/redirect.html");
+});
+
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
