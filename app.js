da2802283ab43b84b1982fe0b025640a92cd97f6
Amit Cohen <amit.c@cheq.ai>
2024-07-29 15:50:34 +0300

    adjusting to any environment

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require("path");
 const { rti, eventsTypes } = require("@cheq.ai/cheq-middlewares");
 const { apiEndpoints } = require("@cheq.ai/cheq-middlewares/config");
 const PORT = process.env.PORT || 5000;
+const baseUrl = process.env.BASE_URL
 const options = {
   apiKey: process.env.API_KEY,
   tagHash: process.env.TAG_HASH,
@@ -16,7 +17,7 @@ const options = {
     blockRedirect: [2, 3, 6, 11, 16, 18, 29, 10], // threat_type 10 = Malicious Bots / threat_type 3 = Automation tool -> bot useragent string: Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html) -> block & redirect (redirectUrl)
     captcha: [4, 5, 13, 15, 17, 7, 14], // threat_type 14 = VPN -> navigate to captcha.html (callback)
   },
-  redirectUrl: `${process.env.BASE_URL}/redirect`,
+  redirectUrl: `${baseUrl}/redirect`,
   callback: function (req, res, next) {
     res.sendFile(path.join(__dirname, "/frontend/captcha.html"));
   },
@@ -29,18 +30,20 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "frontend"));
 
 // Routes
+app.get("/", function (req, res) {
+  res.render("index", { baseUrl });
+});
 app.get("/subscribe", middleware(eventsTypes.SUBSCRIBE), function (req, res) {
   const rtiRes = res.locals.rtiRes;
   const rtiResString = JSON.stringify(rtiRes, null, 2);
 
   res.render("subscribe", { rtiResString });
 });
-
 app.get("/redirect", function (req, res) {
   res.render("redirect");
 });
 
-// Start Server
+// Start
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
